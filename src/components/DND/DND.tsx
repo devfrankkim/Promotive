3db3789d4263ee8c04b62b8bf1bfbb7d9295c81a
heyfranksmile <heyfranksmile@gmail.com>
2022-08-13 19:29:46 +0900

    feat: fixing bugs - same board - DND

--- src/components/DND/DND.tsx
+++ src/components/DND/DND.tsx
@@ -8,62 +8,25 @@ import { useRecoilState, useRecoilValue } from "recoil";
 import styled from "styled-components";
 import { arrayATOM } from "../../atom";
 import Boards from "./Boards";
-import DragCard from "./DragCard";
-
-// setAllBoards((oldList) => {
-//   const newList = [...oldList];
-//   // Remove the current index
-//   newList.splice(source.index, 1);
-//   // Replace the item instead. (옮기려는 index, 0 , 새로운 아이템)
-//   newList.splice(destination.index, 0, draggableId);
-
-//   return newList;
-// });
 
 const DND = () => {
   const [allBoards, setAllBoards] = useRecoilState(arrayATOM);
-  // console.log(allBoards);
 
   const onDragEnd = (info: DropResult) => {
     const { draggableId, source, destination } = info;
 
     if (!destination || !source) return;
 
-    const copyBoards = { ...allBoards };
     const boardFrom = [...allBoards[source.droppableId]];
     const boardTo = [...allBoards[destination.droppableId]];
 
-    // if (source.droppableId === destination.droppableId) {
-    //   boardTo.splice(source.index, 1);
-    // }
+    // Remove duplicate if same board
+    if (source.droppableId === destination.droppableId) {
+      boardTo.splice(source.index, 1);
+    }
 
-    if (
-      source.droppableId === destination.droppableId &&
-      source.index === destination.index
-    )
-      return;
-
-    // console.log("source", source);
-    // console.log("destination", destination);
-
-    console.log(" ====== Before ====== ");
-
-    console.log(boardFrom, source.droppableId, source);
-    console.log(boardTo, destination.droppableId, destination);
-
-    console.log(" ====== After ====== ");
     boardFrom.splice(source.index, 1);
     boardTo.splice(destination.index, 0, draggableId);
-
-    console.log(boardFrom, source.droppableId, source.index);
-    console.log(boardTo, destination.droppableId, destination.index);
-
-    // console.log("key", copyBoards[source.droppableId], source.droppableId);
-    // console.log(
-    //   "key",
-    //   copyBoards[destination.droppableId],
-    //   destination.droppableId
-    // );
 
     setAllBoards((boards) =>
       source.droppableId !== destination.droppableId
@@ -74,14 +37,9 @@ const DND = () => {
           }
         : {
             ...boards,
-            [destination.droppableId]: boardTo,
+            [source.droppableId]: boardTo,
           }
     );
-
-    // if (destination.droppableId === source.droppableId) {
-    //   const copyBoards = { ...allBoards };
-    //   console.log(copyBoards);
-    // }
   };
 
   return (
